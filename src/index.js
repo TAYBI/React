5d0d5976c0e4946d2edbf55f5138d486b60ea687
TAYBI <taybib4@gmail.com>
2020-11-18 11:10:50 +0100

    ll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Index(){
-  return <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, eum.</h1>
+function BookLust(){
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  )
 }
 
-ReactDom.render(<Index/>, document.getElementById('root'));
+const  Book = () => {
+  return (
+    <div className="">
+      <img src="https://images-na.ssl-images-amazon.com/images/I/41HQI3QQOcL._SX327_BO1,204,203,200_.jpg" alt=""/>
+      <h1 className="title">The Testaments: The Sequel to The Handmaid's Tale </h1>
+      <h3 className="author">by Margaret Atwood </h3>
+    </div>
+  )
+}
+
+ReactDom.render(<BookLust/>, document.getElementById('root'));
